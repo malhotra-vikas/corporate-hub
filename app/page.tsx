f9a7a4acb608c70e0eed67d4cdce1cac11407365
Vikas Malhotra <malhotra.vikas@gmail.com>
2025-02-17 11:07:25 -0500

    Fixed footer

--- app/page.tsx
+++ app/page.tsx
@@ -123,13 +123,6 @@ export default function HomePage() {
           </div>
         </div>
       </section>
-      <footer className="border-t py-6 md:py-0">
-        <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
-          <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
-            <img src="/airhub-logo.png" alt={themeConfig.name} className="h-6 w-auto" />
-          </div>
-        </div>
-      </footer>
     </div>
   )
 }
